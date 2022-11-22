188f4cb8d48737b252b3d518ee4644c22589f6e3
Cristina Nicolàs <alumnos@clipplatform.com>
2022-11-22 15:39:30 +0100

    script instalacion contenedor docker

---
+++ Scripts/InstalacionContenedorDocker.ps1
@@ -0,0 +1,13 @@
+﻿#Install-Module -Name BcContainerHelper -Force
+
+$artifactUrl = Get-BCArtifactUrl -country es -select Latest -type Sandbox
+
+New-BcContainer -accept_eula `
+                -artifactUrl $artifactUrl `
+                -auth UserPassword `
+                -containerName BC20CU5ES `
+                -dns 8.8.8.8 `
+                -includeTestLibrariesOnly `
+                -includeTestToolkit `
+                -updateHosts `
+                -useBestContainerOS
