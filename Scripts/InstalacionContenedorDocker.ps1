0ea0b316595ced0ec13c69ce89d3a38a3e6d4303
Cristina Nicolàs <alumnos@clipplatform.com>
2022-11-22 16:03:18 +0100

    script instalacion contenedor docker con nombre de contenedor variable

--- Scripts/InstalacionContenedorDocker.ps1
+++ Scripts/InstalacionContenedorDocker.ps1
@@ -2,10 +2,19 @@
 
 $artifactUrl = Get-BCArtifactUrl -country es -select Latest -type Sandbox
 
+$splittedArtifactURL = $artifactUrl.Split('/')
+$splittedVersion = $splittedArtifactURL[4].Split('.')
+
+$version = $splittedVersion[0]
+$cumulative = $splittedVersion[1]
+$country = $splittedArtifactURL[5]
+
+$containerName = 'BC' + $version + 'CU' + $cumulative + $country
+
 New-BcContainer -accept_eula `
                 -artifactUrl $artifactUrl `
                 -auth UserPassword `
-                -containerName BC20CU5ES `
+                -containerName $containerName `
                 -dns 8.8.8.8 `
                 -includeTestLibrariesOnly `
                 -includeTestToolkit `
